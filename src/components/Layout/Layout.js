0fe269639392680d8b96ded51e7f5af567863cec
chintan global <chintancyber123@gmail.com>
2021-02-12 13:49:16 +0530

    done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Aux from '../../hoc/Auxilary';
 import classes from './Layout.css';
 
+// console.log(classes.Contents)
+
 const layout = (props) => (
     <Aux>
     <div>
         Toolbar,Sidebar,BAckdrop
     </div>
-    <main className={classes.Content} >
+    <main className={classes.Contents}>
         {props.children}
     </main>
     </Aux>
